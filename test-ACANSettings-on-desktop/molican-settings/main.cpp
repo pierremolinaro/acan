14d13d1ec32e2213ee38d67b6c561038249e10ec
Pierre Molinaro <pierremolinaro@MacBookProPierre.local>
2017-10-07 19:15:35 +0200

    Added test ACANSettings on desktop

---
+++ test-ACANSettings-on-desktop/molican-settings/main.cpp
@@ -0,0 +1,145 @@
+//
+//  main.cpp
+//  molican-settings
+//
+//  Created by Pierre Molinaro on 30/09/2017.
+//  Copyright © 2017 Pierre Molinaro. All rights reserved.
+//
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+#include "ACANSettings.h"
+#include "Set.h"
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+#include <iostream>
+using namespace std ;
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+static void compute (const uint32_t inWhishedBaudRate) {
+  ACANSettings settings (inWhishedBaudRate) ;
+  cout << "Wished baud rate: " << settings.mWhishedBitRate << " bit/s"  << endl ;
+  cout << "  Divisor : " << (unsigned) settings.mBitRatePrescaler << endl ;
+  cout << "  Prop seg: " << (unsigned) settings.mPropagationSegment << endl ;
+  cout << "  Segment1: " << (unsigned) settings.mPhaseSegment1 << endl ;
+  cout << "  Segment2: " << (unsigned) settings.mPhaseSegment2 << endl ;
+  cout << "  RJW     : " << (unsigned) settings.mRJW << endl ;
+  cout << "  Sampling: " << (settings.mTripleSampling ? "triple" : "single") << endl ;
+  cout << "  Actual baud rate: " << settings.actualBitRate () << " bit/s" << endl ;
+  cout << "  ppm: " << settings.ppmFromWishedBitRate () << endl ;
+  cout << "  Sample Point: " << settings.samplePointFromBitStart () << "%" << endl ;
+  cout << "  Bit setting ok: " << (settings.mBitSettingOk ? "yes" : "no") << endl ;
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+static void exploreAllSettings (void) {
+  cout << "Explore all settings" << endl ;
+  const uint32_t start = 1 ; // 1 bit/s
+  const uint32_t end = 20 * 1000 * 1000 ; // 20 Mbit/s
+  for (uint32_t br = start ; br <= end ; br ++) {
+    ACANSettings settings (br) ;
+    bool ok = (settings.mBitRatePrescaler >= 1) && (settings.mBitRatePrescaler <= 256) ;
+    if (ok) {
+      ok = (settings.mPropagationSegment >= 1) && (settings.mPropagationSegment <= 8) ;
+    }
+    if (ok) {
+      ok = (settings.mPhaseSegment1 >= 1) && (settings.mPhaseSegment1 <= 8) ;
+    }
+    if (ok) {
+      ok = (settings.mPhaseSegment2 >= 2) && (settings.mPhaseSegment2 <= 8) ;
+    }
+    if (ok) {
+      ok = (settings.mRJW >= 1) && (settings.mRJW <= 4) && (settings.mRJW <= settings.mPhaseSegment2) ;
+    }
+    if (ok) {
+      ok = !settings.mTripleSampling || (settings.mPhaseSegment1 >= 2) ;
+    }
+    if (!ok) {
+      cout << "Error for br : " << br << endl ;
+      exit (1) ;
+    }
+  }
+  cout << "Ok" << endl ;
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+static void allCorrectSettings (const uint32_t inStep, Set <uint32_t> & ioValidSettingSet) {
+  cout << "All valid settings" << endl ;
+  for (uint32_t br = 1000 ; br < 20000000 ; br += inStep) {
+    ACANSettings settings (br) ;
+    if (settings.mBitSettingOk) {
+      ioValidSettingSet.insert (br) ;
+    }
+  }
+  cout << "  Completed, " << ioValidSettingSet.count () << " valid settings" << endl ;
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+//  EXACT SETTINGS
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+static void allExactSettings (Set <uint32_t> & ioExactSettingSet) {
+  cout << "All exact settings" << endl ;
+  for (uint32_t br = 1000 ; br < 20000000 ; br ++) {
+    ACANSettings settings (br, 0) ;
+    if (settings.mBitSettingOk) {
+      ioExactSettingSet.insert (br) ;
+    }
+  }
+  cout << "  Completed, " << ioExactSettingSet.count () << " exact settings" << endl ;
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+static void exhaustiveSearchOfAllExactSettings (Set <uint32_t> & ioExactSettingSet) {
+  const uint32_t kCANClockFrequency = 16 *  1000 * 1000 ; // 16 MHz
+  for (uint32_t brp = 1 ; brp <= 256 ; brp ++) {
+    for (uint32_t TQCount = 5 ; TQCount <= 25 ; TQCount ++) {
+      const uint32_t bitRate = kCANClockFrequency / brp / TQCount ;
+      const bool exact = (bitRate * brp * TQCount) == kCANClockFrequency ;
+      if (exact) {
+        ioExactSettingSet.insert (bitRate) ;
+      }
+    }
+  }
+  cout << "  Exhaustive search completed, " << ioExactSettingSet.count () << " exact settings" << endl ;
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+//   MAIN
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+int main (int /* argc */, const char * /* argv */ []) {
+//  compute (250 * 1000) ;
+//  compute (125 * 1000) ;
+//  compute (500 * 1000) ;
+//  compute (1000 * 1000) ;
+//  compute (10 * 1000) ;
+//  compute (842 * 1000) ;
+//  compute (821 * 1000) ;
+//  compute (2000) ;
+//  compute (20 * 1000 * 1000) ;
+  compute (2509) ;
+//--- Explore all settings
+  exploreAllSettings () ;
+//--- Check valid settings
+  const uint32_t step = 100 ;
+  Set <uint32_t> validSettingSet ;
+  allCorrectSettings (step, validSettingSet) ;
+//--- Check all exact settings
+  Set <uint32_t> exactSettingSet ;
+  allExactSettings (exactSettingSet) ;
+  Set <uint32_t> exhaustiveExactSettingSet ;
+  exhaustiveSearchOfAllExactSettings (exhaustiveExactSettingSet) ;
+  if (exactSettingSet != exhaustiveExactSettingSet) {
+    cout << "  EQUALITY ERROR" << endl ;
+    exit (1) ;
+  }
+  return 0;
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
---
+++ test-ACANSettings-on-desktop/molican-settings/set.h
@@ -0,0 +1,122 @@
+#ifndef _SET_TEMPLATE_DEFINED
+#define _SET_TEMPLATE_DEFINED
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+#include <stdlib.h>
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+template <typename T> class Set {
+//--- Default constructor
+  public: Set (void) ;
+
+//--- Handle copy
+  public: Set (const Set &inOperand) ;
+  public: Set & operator = (const Set & inOperand) ;
+
+//--- Destructor
+  public: ~Set (void) ;
+
+//--- Accessors
+  public : inline size_t count (void) const { return mCount ; }
+  public : inline bool empty (void) const { return mCount == 0 ; }
+  public : inline size_t capacity (void) const { return mCapacity ; }
+
+//--- Set operations
+  public : inline void clear (void) { mCount = 0 ; }
+  public : void insert( const T & inItem) ;
+
+//--- Equality test
+  public: bool operator == (const Set<T> & inOperand) const ;
+  public: bool inline operator != (const Set<T> & inOperand) const { return !(*this == inOperand) ; }
+
+//--- Private properties
+  private: T * mArray = NULL ;
+  private: size_t mCapacity = 0 ;
+  private: size_t mCount = 0 ;
+};
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+template <typename T> Set<T>::Set (void) {
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+template <typename T> Set<T>::Set (const Set & inOperand) {
+  mCount = inOperand.mCount ;
+  mCapacity = inOperand.mCapacity ;
+  mArray = new T [mCount] ;
+  for (size_t i = 0 ; i < mCount ; i++) {
+    mArray[i] = inOperand.mArray [i];
+  }
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+template <typename T> Set<T>::~Set() {
+  delete [] mArray ;
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+template <typename T> Set<T> & Set<T>::operator = (const Set & inOperand) {
+  if (this != & inOperand) {
+    if (mCapacity <= inOperand.mCount) {
+      delete [] mArray ; mArray = new T [inOperand.mCount] ;
+      mCapacity = inOperand.mCount ;
+    }
+    for (size_t i = 0 ; i < inOperand.mCount ; i++) {
+      mArray [i] = inOperand.mArray [i] ;
+    }
+  }
+  return *this ;
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+template <typename T> void Set<T>::insert (const T & inItem) {
+  size_t insertionIndex = 0 ;
+  bool loop = true ;
+  bool found = false ;
+  for (size_t i = 0 ; (i < mCount) && loop ; i++) {
+    if (mArray [i] < inItem) {
+      insertionIndex ++ ;
+    }else if (mArray [i] == inItem) {
+      loop = false ;
+      found = true ;
+    }else{
+      loop = false ;
+    }
+  }
+  if (!found) {
+    if (mCount == mCapacity) {
+      mCapacity ++ ;
+      T * newArray = new T [mCapacity] ;
+      for (size_t i = 0 ; i < mCount ; i++) {
+        newArray [i] = mArray [i] ;
+      }
+      delete [] mArray ; mArray = newArray ;
+    }
+    for (size_t i = mCount ; i>insertionIndex ; i--) {
+      mArray [i] = mArray [i-1] ;
+    }
+    mArray [insertionIndex] = inItem ;
+    mCount ++ ;
+  }
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+template <typename T> bool Set<T>::operator == (const Set<T> & inOperand) const {
+  bool equal = mCount == inOperand.mCount ;
+  for (size_t i=0 ; (i<mCount) && equal ; i++) {
+    equal = mArray [i] == inOperand.mArray [i] ;
+  }
+  return equal ;
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+#endif
